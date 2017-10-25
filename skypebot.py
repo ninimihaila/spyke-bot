acefee65457ee27128c3eb923687d528d3370d3b
Sabin Mihaila <sabin.mihaila@tss-yonder.com>
2017-10-25 15:18:49 +0300

    Updated so it can be used with latest skype version

--- skypebot.py
+++ skypebot.py
@@ -4,14 +4,17 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
 import config
+import pickle
 from messages.generators.build_generators import build_markov
 
-user_agent = (
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36"
-)
 
-driver = webdriver.Firefox()
+driver = webdriver.Firefox()  # change if you need to use chrome
 driver.set_window_size(1024, 768)
+
+
+markov = build_markov(author='', json_files=[
+    # write the paths to your json files here
+])
 
 
 def wait(secs, message=''):
@@ -26,11 +29,15 @@ def wait(secs, message=''):
 
 def login_to_skype(driver):
     driver.get('https://web.skype.com/')
-    wait(7)
+    wait(3)
     driver.find_element_by_css_selector('#username').send_keys(config.SKYPE['username'])
-    driver.find_element_by_css_selector('#password').send_keys(config.SKYPE['password'])
-    driver.find_element_by_css_selector('#password').send_keys('\n')
-    wait(7, 'waiting for skype to load...')
+    driver.find_element_by_css_selector('#signIn').click()
+    wait(3)
+    driver.find_element_by_css_selector('.placeholder').click()
+    driver.find_element_by_css_selector('.placeholder').send_keys(config.SKYPE['password'])
+    driver.find_element_by_css_selector('.btn-primary').click()
+    wait(10, 'waiting for skype to load...')
+
     # print console log messages
     # import json
     # messages = json.loads(list(driver.get_log('har'))[0]['message'])['log']['entries']
@@ -42,19 +49,73 @@ def login_to_skype(driver):
 
 
 def send_skype_message(driver, message):
+    if not message:
+        return
+
     xpath = config.SKYPE['conversation_xpath'].format(config.SKYPE['conversation_title'])
     driver.find_element_by_xpath(xpath).click()
-    time.sleep(5)
+
     # click on the last textarea
-    driver.find_elements_by_css_selector('#textarea-bindings textarea')[-1].click()
-    time.sleep(3)
-    ActionChains(driver).send_keys(message).send_keys('\n').perform()
-    time.sleep(3)
+    chat_area = driver.find_element_by_css_selector('#chatInputAreaWithQuotes')
+    chat_area.click()
+    chat_area.send_keys(message)
+    chat_area.send_keys('\n')
+
+    driver.find_element_by_css_selector('.send-button').click()
+
+
+reply_dict = {
+    'say something': markov.generate,
+    # add other custom commands and replies here
+}
+
+def process_message(message):
+    for key in reply_dict:
+        if key.lower() in message['message'].lower():
+            return reply_dict[key]()
+
+
+def debug_signal_handler(signal, frame):
+    import pudb;pu.db
+
+import signal
+signal.signal(signal.SIGINT, debug_signal_handler)
 
 
 if __name__ == '__main__':
     driver.implicitly_wait(10)
     login_to_skype(driver)
-    m = build_markov('', json_files=[])  # write the paths to your json files here
-    send_skype_message(driver, m.generate(size=None))
+    # send_skype_message(driver, m.generate(size=None))
+    handled = set()
+    number_of_messages = 5
+    print('Begin listening for messages')
+    listening = False
+    while True:
+        wait(3)
+        try:
+            last_messages = []
+            for message in driver.find_elements_by_css_selector('.content')[-number_of_messages:]:
+                processed_message = {}
+                name_els = message.find_elements_by_xpath('..//a')
+                name = name_els[0].get_attribute('textContent').strip() if name_els else 'self'
+                processed_message['name'] = name
+                processed_message['message'] = message.get_attribute('textContent').strip()
+                msg_time = (
+                    message.find_element_by_xpath('..//div[contains(@class, "timestamp")]')
+                    .get_attribute('textContent')
+                    .strip()[-8:]
+                )
+                processed_message['time'] = msg_time
+                processed_message['id'] = message.get_attribute('id')[4:]
+                last_messages.append(processed_message)
+
+            # import pudb;pu.db
+            for message in last_messages:
+                if message['id'] not in handled and config.SKYPE['callname'] in message['message'].lower():
+                    handled.add(message['id'])
+                    send_skype_message(driver, process_message(message))
+        except Exception as ex:
+            import pudb;pu.db
+            print('An error occured: ', ex)
+
     driver.close()
